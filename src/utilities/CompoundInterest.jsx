057b05db601f5d54488a8922a450ada6b79bd552
btsween <47014701+btsween@users.noreply.github.com>
2021-03-01 21:40:52 -0500

    SET-UP: cleaning, standalone transition (#2)
    * SET-UP: removing PWS work for standalone application
    
    * SET-UP: use state on form input

--- src/utilities/CompoundInterest.jsx
+++ src/utilities/CompoundInterest.jsx
@@ -1,11 +1,37 @@
-export const createInterestInfo = (inital, monthly, time, rate) => {
-  let yVals = [inital];
+export const createInterestInfo = (initial, monthly, time, rate) => {
+  // TODO: need to parse these before input...
+  // TODO: formula for interest calculation?
+  // let i = parseInt(initial);
+  // let t = parseInt(time);
+  // let r = parseInt(rate);
+  // let m = parseInt(monthly);
+  // r = r / 100 / 12;
+
+  console.log(initial + 200);
+  console.log(monthly + 200);
+  console.log(time + 200);
+  console.log(rate + 200);
+
+  let yVals = [initial];
   let xVals = [0];
-  let current = inital;
-  for (let i = 1; i <= time; i++) {
-    current = (1 + rate / 100) * current;
-    xVals.push(i);
-    yVals.push(current);
+  // let current = i;
+
+  // for (let idx = 1; idx <= t * 12; idx++) {
+  //   current += m;
+  //   current += current * 0.005;
+  //   if (idx % 12 === 0) {
+  //     xVals.push(idx / 12);
+  //     yVals.push(current);
+  //   }
+  // }
+  let current = initial;
+  for (let idx = 1; idx <= time * 12; idx++) {
+    current += monthly;
+    current += (current * rate) / 100 / 12;
+    if (idx % 12 === 0) {
+      xVals.push(idx / 12);
+      yVals.push(current);
+    }
   }
   return {
     labels: xVals,
