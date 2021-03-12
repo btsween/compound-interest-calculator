d280d12bfcfe50c1164e3d1ee521f8dd2a7dafef
Bryan Sweeney <bryan.sweeney@uconn.edu>
2021-03-11 21:05:49 -0500

    DEPLOYMENT: Dynamic port listen for Heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const path = require('path');
+const PORT = process.env.NODE_ENV || "3000";
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(9000);
+app.listen(PORT);
